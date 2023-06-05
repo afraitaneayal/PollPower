7121ab578490b88d99a53df026968ba1da32cf1c
Ayal Afraitane <afraitaneayal0@.com>
2023-06-05 16:59:11 +0000

    some screen added

---
+++ android/app/src/main/kotlin/com/example/poll_power/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poll_power
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:poll_power/root.dart';
+
+void main() {
+  runApp(const Root());
+}

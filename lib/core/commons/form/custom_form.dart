c27abe57854e5e3c34611d07b508e664c7707cc3
yayahc <hamedcuenca5@gmail.com>
2023-06-09 12:56:29 +0000

    form added

---
+++ lib/core/commons/form/custom_form.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:poll_power/core/extensions/extension_on_strings.dart';
+
+import '../../../features/auth/user_entity.dart';
+import '../../../services/user/isar_services.dart';
+
+class CustomForm extends StatefulWidget {
+  const CustomForm({super.key});
+
+  @override
+  State<CustomForm> createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<CustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? firstNameValue;
+  String? lastNameValue;
+  String? gradeValue;
+  String? areaOfStudyValue;
+  String? passwordValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(children: _buidFormField()),
+    );
+  }
+
+  List<Widget> _buidFormField() {
+    return [
+      TextFormField(
+        decoration: const InputDecoration(labelText: "First Name"),
+        onChanged: (String value) {
+          setState(() {
+            firstNameValue = value;
+          });
+        },
+      ),
+      TextFormField(
+        decoration: const InputDecoration(labelText: "Last Name"),
+        onChanged: (String value) {
+          setState(() {
+            lastNameValue = value;
+          });
+        },
+      ),
+      TextFormField(
+        decoration: const InputDecoration(labelText: "Grade"),
+        onChanged: (String value) {
+          setState(() {
+            gradeValue = value;
+          });
+        },
+      ),
+      TextFormField(
+        decoration: const InputDecoration(labelText: "Area of Study"),
+        onChanged: (String value) {
+          setState(() {
+            areaOfStudyValue = value;
+          });
+        },
+      ),
+      _buildElevateButton()
+    ];
+  }
+
+  Widget _buildElevateButton() {
+    return ElevatedButton(
+        onPressed: () {
+          final isarService = IsarServices();
+          isarService.createUser(User()
+            ..fistName = firstNameValue
+            ..lastName = lastNameValue
+            ..grade = gradeValue
+            ..arreaOfStudy = areaOfStudyValue);
+        },
+        child: "login".getWidget());
+  }
+}
--- lib/core/screens/register/register_user_screen.dart
+++ lib/core/screens/register/register_user_screen.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:poll_power/core/extensions/extension_on_strings.dart';
-import 'package:poll_power/features/auth/user_entity.dart';
-import 'package:poll_power/services/user/isar_services.dart';
+import 'package:poll_power/core/commons/form/custom_form.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TextButton(
-              onPressed: () {
-                final isarService = IsarServices();
-                isarService.createUser(User()..fistName = "Yaya Hc");
-              },
-              child: "Register".getWidget())
-        ],
-      ),
+    return const Scaffold(
+      body: CustomForm(),
     );
   }
 }

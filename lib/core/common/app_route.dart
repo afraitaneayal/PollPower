89ccbae80d4063a51ebfe75ceecfc95cc5c6a606
yayahc <hamedcuenca5@gmail.com>
2024-03-24 04:37:06 +0000

    app setup according to clean code arch

---
+++ lib/core/common/app_route.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static String splash = "/splash";
+  static String login = "/login";
+  static String register = "/register";
+  static String accountType = "/accountType";
+  static String home = "/";
+  static String candidate = "/condidate";
+}
--- lib/router.dart
+++ lib/router.dart
@@ -6,53 +6,45 @@ import 'package:poll_power/features/vote/screens/home_screen.dart';
 import 'package:poll_power/features/vote/screens/login_screen.dart';
 import 'package:poll_power/features/vote/screens/register_screen.dart';
 import 'package:poll_power/features/vote/screens/splash_screen.dart';
+import 'core/common/app_route.dart';
 
 final GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
 
 class AppRouter {
   final GoRouter _router = GoRouter(
-      initialLocation: AppRoute.home,
+      initialLocation: AppRoutes.home,
       routes: <RouteBase>[
         GoRoute(
-          path: AppRoute.splash,
+          path: AppRoutes.splash,
           builder: (BuildContext context, GoRouterState state) =>
               const SplashScreen(),
         ),
         GoRoute(
-          path: AppRoute.login,
+          path: AppRoutes.login,
           builder: (context, state) => const LoginScreen(),
         ),
         GoRoute(
-          path: AppRoute.login,
+          path: AppRoutes.login,
           builder: (context, state) => const LoginScreen(),
         ),
         GoRoute(
-          path: AppRoute.accountType,
+          path: AppRoutes.accountType,
           builder: (context, state) => const AccountTypeScreen(),
         ),
         GoRoute(
-          path: AppRoute.register,
+          path: AppRoutes.register,
           builder: (context, state) => const RegisterScreen(),
         ),
         GoRoute(
-          path: AppRoute.home,
+          path: AppRoutes.home,
           builder: (context, state) => const HomeScreen(),
         ),
         GoRoute(
-          path: AppRoute.candidate,
+          path: AppRoutes.candidate,
           builder: (context, state) => const CandidateScreen(),
         ),
       ],
       debugLogDiagnostics: true);
 
   GoRouter get router => _router;
-}
-
-class AppRoute {
-  static String splash = "/splash";
-  static String login = "/login";
-  static String register = "/register";
-  static String accountType = "/accountType";
-  static String home = "/";
-  static String candidate = "/condidate";
 }

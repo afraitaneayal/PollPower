a832e9ee6baba492fa3d53c77037c03eb2070398
yayahc <hamedcuenca5@gmail.com>
2025-03-03 08:32:29 -0800

    chore: remove pub lock
    Co-authored-by: Yaya HC <hamedcuenca5@gmail.com>
    Co-authored-by: AFRAITANE Ayal <afraitaneayal0@gmail.com>

---
+++ autoco.sh
@@ -0,0 +1,9 @@
+cd PollPower
+rm -rf pubscpec.lock
+git add .
+git commit -m "chore: remove pub lock
+
+Co-authored-by: Yaya HC <hamedcuenca5@gmail.com>
+Co-authored-by: AFRAITANE Ayal <afraitaneayal0@gmail.com>"
+git push
+
